4372b1e8f7087d6ed5ddbdf3dd3ae56de1878090
WhiteLightningGun <109026202+WhiteLightningGun@users.noreply.github.com>
2023-03-13 20:10:17 +0000

    changes

--- js/ContactValidation.js
+++ js/ContactValidation.js
@@ -1,9 +1,14 @@
 var email = document.getElementById("email");
+var errorBox = document.getElementById("ErrorBox");
 
 email.oninvalid = function(e) {
     e.target.setCustomValidity("");
     if (!e.target.validity.valid)
     {
-        e.target.setCustomValidity("Hey, fix the email first!!!");
+        e.target.setCustomValidity("Please input valid email address");
     } 
+
+    errorBox.innerHTML = "Please input valid email address";
+
+
 }
