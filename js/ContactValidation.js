26739a5901c1aa0eada5d01ed8909a809da940e2
WhiteLightningGun <109026202+WhiteLightningGun@users.noreply.github.com>
2023-03-13 19:38:33 +0000

    changes

---
+++ js/ContactValidation.js
@@ -0,0 +1,9 @@
+var email = document.getElementById("email");
+
+email.oninvalid = function(e) {
+    e.target.setCustomValidity("");
+    if (!e.target.validity.valid)
+    {
+        e.target.setCustomValidity("Hey, fix the email first!!!!!!1111!");
+    } 
+}
